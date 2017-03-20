f1f8d1d56fa7b31d2f81cdf1f37712da092f9d5c
Daniel Burke <daniel.burke@hmhco.com>
2017-03-20 17:32:10 +0000

    Updated Github configuration

--- github/config.py
+++ github/config.py
@@ -2,17 +2,8 @@ from hubcommander.auth_plugins.enabled_plugins import AUTH_PLUGINS
 
 # Define the organizations that this Bot will examine.
 ORGS = {
-    "Real_Org_Name_here": {
+    "hmhco": {
         "aliases": [
-            "some_alias_for_your_org_here"
-        ],
-        "public_only": False,   # False means that your org supports Private repos, True otherwise.
-        "new_repo_teams": [  # This is a list, so add all the teams you want to here...
-            {
-                "id": "0000000",        # The team ID for the team that you want new repos to be attached to
-                "perm": "push",         # The permission, either "push", "pull", or "admin"...
-                "name": "TeamName"      # The name of the team here...
-            }
         ]
     }
 }
